1da5ce4b41c0daf695abeffa13f84e7edcbe8f0f
CAGER-YT <info.sandytech@gmail.com>
2024-12-20 18:37:09 +0530

    first commit

---
+++ src/main/java/com/example/demo/AuthServiceImpl.java
@@ -0,0 +1,31 @@
+package com.example.demo;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+
+import lombok.AllArgsConstructor;
+
+@Service
+@AllArgsConstructor
+public class AuthServiceImpl implements AuthService {
+
+    private AuthenticationManager authenticationManager;
+
+    @Override
+    public String login(LoginDto loginDto) {
+
+        Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
+                loginDto.getUsernameOrEmail(),
+                loginDto.getPassword()
+        ));
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        String token = JwtTokenProvider.generateToken(authentication);
+
+        return token;
+    }
+}
